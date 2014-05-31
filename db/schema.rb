4f87fb1d98fba7d7bf99f7184da998422b8a9198
vagrant <vagrant@kellogg-dev-box>
2014-05-31 18:52:51 +0000

    Updated Models and Seeds

---
+++ db/schema.rb
@@ -0,0 +1,55 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140531185151) do
+
+  create_table "cars", force: true do |t|
+    t.string  "make"
+    t.string  "model"
+    t.integer "msrp"
+    t.integer "hp"
+    t.string  "drive_type"
+    t.integer "electric_range"
+    t.integer "gas_range"
+    t.integer "total_range"
+    t.float   "hours_to_charge"
+    t.string  "front_photo_url"
+    t.string  "back_photo_url"
+    t.string  "side_photo_url"
+    t.string  "quarter_photo_url"
+  end
+
+  create_table "powers", force: true do |t|
+    t.string "type"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer "car_id"
+    t.integer "user_id"
+    t.text    "comment"
+  end
+
+  create_table "trips", force: true do |t|
+    t.integer "user_id"
+    t.integer "day"
+    t.integer "miles"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "username"
+    t.string "password"
+    t.string "firstname"
+    t.string "lastname"
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,38 @@ User.delete_all
 jc = User.create("username" => "jazzyjeff", "password" => "hockey", "firstname" => "Jeff", "lastname" => "Cohen")
 be = User.create("username" => "breezybrian", "password" => "baseball", "firstname" => "Brian", "lastname" => "Eng")
 
+Power.delete_all
+el = Power.create("type" => "Electric Only")
+eg = Power.create("type" => "Electric & Gas")
+go = Power.create("type" => "Gas Only")
+hy = Power.create("type" => "Hybrid Gas/Electric")
+
 Car.delete_all
-cv =
-nl =
-tm =
-
-
-Fuel.delete_all
-
-
+cv = Car.create("make" => "Chevy", "model" => "Volt", "msrp" => 34185, "hp" => 149, "drive_type" => eg.type, "electric_range" => 38, "gas_range" => 342, "total_range" => 380, "hours_to_charge" => 4, "front_photo_url" => "http://media.ed.edmunds-media.com/chevrolet/volt/2014/evox/2014_chevrolet_volt_4dr-hatchback_base_fs_evox_1_500.jpg", "back_photo_url" => "http://media.ed.edmunds-media.com/chevrolet/volt/2014/evox/2014_chevrolet_volt_4dr-hatchback_base_rst_evox_1_500.jpg", "side_photo_url" => "http://media.ed.edmunds-media.com/chevrolet/volt/2014/evox/2014_chevrolet_volt_4dr-hatchback_base_pr_evox_1_500.jpg", "quarter_photo_url" => "http://media.ed.edmunds-media.com/chevrolet/volt/2014/evox/2014_chevrolet_volt_4dr-hatchback_base_fqh_evox_1_500.jpg")
+nl = Car.create("make" => "Nissan", "model" => "Leaf", "msrp" => 28980, "hp" => 107, "drive_type" => el.type, "electric_range" => 84, "gas_range" => 0, "total_range" => 84, "hours_to_charge" => 5, "front_photo_url" => "http://media.ed.edmunds-media.com/nissan/leaf/2014/evox/2014_nissan_leaf_4dr-hatchback_s_fs_evox_1_500.jpg", "back_photo_url" => "http://media.ed.edmunds-media.com/nissan/leaf/2014/evox/2014_nissan_leaf_4dr-hatchback_s_rst_evox_1_500.jpg", "side_photo_url" => "http://media.ed.edmunds-media.com/nissan/leaf/2014/evox/2014_nissan_leaf_4dr-hatchback_s_pr_evox_1_500.jpg", "quarter_photo_url" => "http://media.ed.edmunds-media.com/nissan/leaf/2014/evox/2014_nissan_leaf_4dr-hatchback_s_fqh_evox_1_500.jpg")
+tm = Car.create("make" => "Tesla", "model" => "Model S", "msrp" => 69570, "hp" => 362, "drive_type" => el.type, "electric_range" => 265, "gas_range" => 0, "total_range" => 265, "hours_to_charge" => 9, "front_photo_url" => "http://media.ed.edmunds-media.com/tesla/model-s/2013/evox/2013_tesla_model-s_sedan_performance_fs_evox_1_500.jpg", "back_photo_url" => "http://media.ed.edmunds-media.com/tesla/model-s/2013/evox/2013_tesla_model-s_sedan_performance_rst_evox_1_500.jpg", "side_photo_url" => "http://media.ed.edmunds-media.com/tesla/model-s/2013/evox/2013_tesla_model-s_sedan_performance_pr_evox_1_500.jpg", "quarter_photo_url" => "http://media.ed.edmunds-media.com/tesla/model-s/2013/evox/2013_tesla_model-s_sedan_performance_fqh_evox_1_500.jpg")
 
 Trip.delete_all
-
+Trip.create("user_id" => jc.id, "day" => 1, "miles" => 34)
+Trip.create("user_id" => jc.id, "day" => 2, "miles" => 12)
+Trip.create("user_id" => jc.id, "day" => 3, "miles" => 51)
+Trip.create("user_id" => jc.id, "day" => 4, "miles" => 70)
+Trip.create("user_id" => jc.id, "day" => 5, "miles" => 0)
+Trip.create("user_id" => jc.id, "day" => 6, "miles" => 12)
+Trip.create("user_id" => jc.id, "day" => 7, "miles" => 19)
+Trip.create("user_id" => be.id, "day" => 1, "miles" => 42)
+Trip.create("user_id" => be.id, "day" => 2, "miles" => 10)
+Trip.create("user_id" => be.id, "day" => 3, "miles" => 0)
+Trip.create("user_id" => be.id, "day" => 4, "miles" => 0)
+Trip.create("user_id" => be.id, "day" => 5, "miles" => 102)
+Trip.create("user_id" => be.id, "day" => 6, "miles" => 24)
+Trip.create("user_id" => be.id, "day" => 7, "miles" => 38)
 
 Review.delete_all
-
-
-
-
+Review.create("car_id" => tm.id, "user_id" => jc.id, "comment" => "Shocking performance!")
+Review.create("car_id" => tm.id, "user_id" => be.id, "comment" => "This car's got spark.")
+Review.create("car_id" => cv.id, "user_id" => jc.id, "comment" => "Keeps up with the current of traffic.")
+Review.create("car_id" => nl.id, "user_id" => be.id, "comment" => "Drives like lightning.")
 
 
 # This file should contain all the record creation needed to seed the database with its default values.
