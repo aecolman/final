07103087accdc82a80ea4792a83f6c4b41b0a17e
vagrant <vagrant@kellogg-dev-box>
2014-05-31 18:09:04 +0000

    First commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,27 @@
+User.delete_all
+jc = User.create("username" => "jazzyjeff", "password" => "hockey", "firstname" => "Jeff", "lastname" => "Cohen")
+be = User.create("username" => "breezybrian", "password" => "baseball", "firstname" => "Brian", "lastname" => "Eng")
+
+Car.delete_all
+cv =
+nl =
+tm =
+
+
+Fuel.delete_all
+
+
+
+Trip.delete_all
+
+
+Review.delete_all
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
