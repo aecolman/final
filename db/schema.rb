86e1c21b10ca13bd378c6989b230de37387d512e
vagrant <vagrant@kellogg-dev-box>
2014-06-09 06:37:13 +0000

    Final Version. Really.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140608042234) do
+ActiveRecord::Schema.define(version: 20140609063558) do
 
   create_table "cars", force: true do |t|
     t.string  "make"
